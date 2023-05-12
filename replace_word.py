80b0ac1130880086cb00a4c77f4115d1513f2f74
esgarov <elchinasgarov@outlook.com>
2023-05-12 15:56:47 +0200

    Comments are added

--- replace_word.py
+++ replace_word.py
@@ -3,10 +3,15 @@
 import os
 import sys
 
+# get the old and new word and the file path from command line arguments
 word_old = sys.argv[1]
 word_new = sys.argv[2]
 file = sys.argv[3]
 
+print(f"word_old: {word_old}")
+print(f"word_new: {word_new}")
+
+# function to find all occurrences of a word in a text
 def findall(text, word):
     result = []
     last_idx = 0
@@ -18,22 +23,30 @@ def findall(text, word):
         last_idx = start_idx + len(word)
     return result
 
+# iterate over all files in the directory tree rooted at the given file path
 for root, dirs, files in os.walk(file):
     for f_name in files:
         f_path = os.path.join(root, f_name)
         print(f_path)
         try:
+            # read the file content
             with open(f_path, 'r') as f:
                 content = f.read()
+
+            # find all occurrences of the old word in the file content
             indexs = findall(content, word_old)
+
             if indexs:
+                # if the old word exists, replace it with the new word and write the new content back to the file
                 with open(f_path, 'w') as f:
                     for start_index in indexs:
                         end_index = start_index + len(word_old)
                         new_content = content[:start_index] + word_new + content[end_index:]
                         content = new_content
                     f.write(new_content)
+
         except:
+            # ignore any errors that occur while reading or writing a file
             pass
 
 
